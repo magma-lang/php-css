b3574302a1d80795d14112298e2ad8b41efe712f
Sören Meier <16178877+soerenmeier@users.noreply.github.com>
2019-07-10 11:22:45 +0200

    Update init.php

--- init.php
+++ init.php
@@ -4,13 +4,11 @@ ini_set( 'display_errors', true );
 define( 'START_TIME', microtime( true ) );
 
 define( 'DIR', __DIR__. DIRECTORY_SEPARATOR );
-
 require_once( DIR. 'engine.php' );
 
-$minifier = new MagmaCss\Engine( DIR. 'tmp/', true );
+$engine = new MagmaCSS\Engine( DIR. 'tmp/', true );
 
-// $file = $minifier->js( [ DIR. 'js/babel.js' ], '1' );
-$file = $minifier->go( DIR. 'css/test.mgcss' );
+$file = $engine->go( DIR. 'css/test.mgcss' );
 
 readfile( DIR. 'tmp/'. $file );
 
